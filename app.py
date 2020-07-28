1327942187398a999652da5d0acc9e00abba518d
Theo LEBRUN <mib.ftw@gmail.com>
2020-07-28 14:52:59 -0400

    Add tag

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
+from aws_cdk.core import Tag
 
 from twitter_trends.twitter_trends_stack import TwitterTrendsStack
 
-
 app = core.App()
-TwitterTrendsStack(app, "twitter-trends")
-
+stack = TwitterTrendsStack(app, 'twitter-trends')
+Tag.add(stack, 'ippon:owner', 'tlebrun')
 app.synth()
