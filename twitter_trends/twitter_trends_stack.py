a327bdfd3b819e727c41c6fe34f55fd8400e89eb
Theo LEBRUN <mib.ftw@gmail.com>
2020-07-28 14:54:39 -0400

    Add CDK code

--- twitter_trends/twitter_trends_stack.py
+++ twitter_trends/twitter_trends_stack.py
@@ -1,4 +1,10 @@
-from aws_cdk import core
+from aws_cdk import (
+    aws_s3 as s3,
+    aws_events as events,
+    aws_lambda as lambda_,
+    aws_events_targets as targets,
+    core,
+)
 
 
 class TwitterTrendsStack(core.Stack):
@@ -6,4 +12,23 @@ class TwitterTrendsStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        bucket = s3.Bucket(self, 'tlebrun-playground')
+
+        handler = lambda_.Function(self, 'TrendsHandler',
+                                   runtime=lambda_.Runtime.PYTHON_3_8,
+                                   code=lambda_.Code.asset('resources'),
+                                   handler='trends.main',
+                                   environment=dict(BUCKET=bucket.bucket_name,
+                                                    CONSUMER_KEY='',
+                                                    CONSUMER_SECRET='',
+                                                    ACCESS_TOKEN_KEY='',
+                                                    ACCESS_TOKEN_SECRET='',
+                                                    OEID='')
+                                   )
+
+        bucket.grant_read_write(handler)
+
+        events.Rule(self, 'RuleHandler',
+                    rule_name='Twitter_Trends_15_Minutes_Rule',
+                    schedule=events.Schedule.rate(duration=core.Duration.minutes(15)),
+                    targets=[targets.LambdaFunction(handler)])
